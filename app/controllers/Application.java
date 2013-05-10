ae1f343d0adaccef033fee3d731fde42ef9ea2b8
emeraldjava <paul.t.oconnell@gmail.com>
2013-05-10 14:21:31 +0100

    Add a form and mapping for the tasks method.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,21 @@
 package controllers;
 
+import models.Task;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
   
+	static Form<Task> taskForm = Form.form(Task.class);
+	
     public static Result index() {
     	return redirect(routes.Application.tasks());
         //return ok("emeraldjava-play application.");
     }
     
 	public static Result tasks() {
-		return TODO;
+		return ok(views.html.index.render(Task.all(), taskForm));
 	}
 
 	public static Result newTask() {
--- app/models/Task.java
+++ app/models/Task.java
@@ -3,9 +3,13 @@ package models;
 import java.util.ArrayList;
 import java.util.List;
 
+import play.data.validation.Constraints.Required;
+
 public class Task {
 
 	public Long id;
+	
+	@Required
 	public String label;
 
 	public static List<Task> all() {
